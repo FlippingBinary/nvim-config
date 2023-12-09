aa4ffeba5d28746a3ad68b5c1bf852dfc11f90d0
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2023-12-09 17:56:42 -0500

    feat(treesitter): Require taplo and eslint

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,12 +16,14 @@ return {
       "bash", -- because sometimes you need to bash your head against the wall
       "c", -- because you can't spell classic without c
       "css", -- because colors make everything better
+      "eslint", -- because javascript needs some discipline
       "javascript", -- because who doesn't love a good semicolon joke
       "json", -- because it's just so on point
       "lua", -- because it's the moon of programming languages
       "python", -- because indentation is not a suggestion
       "rust", -- because memory safety is no laughing matter
       "svelte", -- because it's the new kid on the block
+      "taplo", -- because taplo is the new kid on the block
       "toml", -- because it's not yaml
       "tsx", -- because typescript + jsx = tsx
       "typescript", -- because javascript needs some discipline
