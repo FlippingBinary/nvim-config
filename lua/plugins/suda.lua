328c4b2e77f6ee53a8df3026cc685f839eb1e3e5
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2023-12-13 16:21:44 -0500

    feat: Assign Alt-S to SudaWrite

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -1,10 +1,7 @@
 -- Make it possible to sudo save a file without losing changes
 return {
   "lambdalisue/suda.vim",
-  -- keys = {
-  --   { "i", "<A-s>", ":SudaWrite<cr>", { desc = "Save file with sudo" } },
-  --   { "x", "<A-s>", ":SudaWrite<cr>", { desc = "Save file with sudo" } },
-  --   { "n", "<A-s>", ":SudaWrite<cr>", { desc = "Save file with sudo" } },
-  --   { "s", "<A-s>", ":SudaWrite<cr>", { desc = "Save file with sudo" } },
-  -- },
+  keys = {
+    { "<A-s>", ":SudaWrite<cr>", mode = { "i", "n" }, desc = "Save file with sudo" },
+  },
 }
