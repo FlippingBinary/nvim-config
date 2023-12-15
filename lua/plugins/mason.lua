d158e11e6c6c36af160f9068e197c8523141426f
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2023-12-14 19:24:15 -0500

    feat: Put a border around popup elements

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        -- Put a border around the Mason window, separating it from the text behind it.
+        border = "rounded",
+      },
+    },
+  },
+}
