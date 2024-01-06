bca9b0d06f4d012656c9c16c9ebb6d8d28b9b11d
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-01-06 13:44:48 -0500

    feat: Identify current OS so configuration can be customized per OS

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,37 @@
 
 -- Disable LazyVim auto format
 vim.g.autoformat = false
+
+-- Identify the current OS, setting global variables that can be used by plugins and other configuration files.
+local function identify_os()
+  local uname = vim.loop.os_uname()
+  if uname.sysname == "Windows_NT" then
+    vim.g.os_family = "windows"
+  elseif uname.sysname == "Darwin" then
+    vim.g.os_family = "mac"
+  elseif uname.sysname == "Linux" then
+    vim.g.os_family = "linux"
+  else
+    print(
+      "A new OS! Awesome! Please identify what os_family '"
+        .. uname.sysname
+        .. "' belongs to and update ~/.config/nvim/lua/config/options.lua!"
+    )
+  end
+
+  if os.getenv("WSL_DISTRO_NAME") then
+    -- We're running on Windows under WSL, so it's probably Linux, but there may be special changes that have to be made with special circumstances
+    vim.g.os_platform = "wsl"
+  -- setup_zsh()
+  else
+    -- If the `/.dockerenv` file exists, we should assume we're running under docker.
+    local docker_env = io.open("/.dockerenv")
+    if docker_env then
+      docker_env:close()
+      vim.g.os_platform = "docker"
+    else
+      vim.g.os_platform = "native"
+    end
+  end
+end
+identify_os()
