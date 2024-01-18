36abc7e3df92de7ad2903ae11b6c05673ae3e1f9
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-01-18 16:37:05 -0500

    feat: Replace mini.comment with Comment.nvim for block comment support

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,12 @@
+return {
+  { "echasnovski/mini.comment", enabled = false },
+
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
+}
