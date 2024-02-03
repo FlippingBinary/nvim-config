548a6dccdfcded1e25ba99f6367597a2043ea051
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-02-03 12:30:32 -0500

    feat: Automatically pull changes to config repository

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
+-- Synchronize this directory with the latest changes from the remote repository
+local configpath = vim.fn.stdpath("config")
+local config_pull_result = vim.fn.system({ "git", "-C", configpath, "pull" })
+if config_pull_result ~= "Already up to date.\n" then
+  print("Checking for config updates: '" .. config_pull_result .. "'")
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
