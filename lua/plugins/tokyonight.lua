c612e872241c3083686b8fc08ac26078c8d4ae19
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-02-11 19:40:24 -0500

    fix: Enable transparent background only on native Linux

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,10 +1,17 @@
-return {
+-- Common settings for TokyoNight
+local plugin = {
   "folke/tokyonight.nvim",
-  opts = {
+}
+
+if vim.g.os_family == "linux" and vim.g.os_platform == "native" then
+  -- Enable transparent background only on native Linux
+  plugin.opts = {
     transparent = true,
     styles = {
       sidebars = "transparent",
       floats = "transparent",
     },
-  },
-}
+  }
+end
+
+return plugin
