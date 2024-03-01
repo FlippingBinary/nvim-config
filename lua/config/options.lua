b03e5e68d08520935fc483e325dc6931fd6c8272
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-03-01 10:34:37 -0500

    feat: Replace the command prompt with PowerShell on Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -42,3 +42,27 @@ vim.wo.cursorcolumn = true
 vim.wo.colorcolumn = "78,100"
 -- Increase the keymap timeout so that `gcc` is a little easier to use
 vim.o.timeoutlen = 500
+
+-- Configure PowerShell if on Windows
+if vim.g.os_family == "windows" then
+  -- Check if 'pwsh' is executable and set the shell accordingly
+  if vim.fn.executable("pwsh") == 1 then
+    vim.o.shell = "pwsh"
+  else
+    vim.o.shell = "powershell"
+  end
+
+  -- Setting shell command flags
+  vim.o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+  -- Setting shell quote options
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+end
