cd898f6804134dc57b08bc28c5087cb191a7a8bc
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-04-27 16:44:39 -0400

    fix: Brighten highlighting of disabled code slightly so it's as readable as a comment

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,17 +1,26 @@
 -- Common settings for TokyoNight
 local plugin = {
   "folke/tokyonight.nvim",
+  opts = {
+    on_highlights = function(highlights, colors)
+      highlights.DiagnosticUnnecessary = {
+        fg = colors.comment,
+      }
+    end,
+  },
 }
 
 if vim.g.os_family == "linux" and vim.g.os_platform == "native" then
-  -- Enable transparent background only on native Linux
-  plugin.opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  return vim.tbl_deep_extend("error", plugin, {
+    -- Enable transparent background only on native Linux
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
-  }
+  })
+else
+  return plugin
 end
-
-return plugin
