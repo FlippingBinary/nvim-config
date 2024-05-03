47594c84583d7f4196936a5ca0798fed2766b27c
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-05-03 08:32:24 -0400

    feat: Use `nvim-notify` instead of `print` for config update errors

--- init.lua
+++ init.lua
@@ -1,9 +1,16 @@
 -- Synchronize this directory with the latest changes from the remote repository
 local configpath = vim.fn.stdpath("config")
 local config_pull_result = vim.fn.system({ "git", "-C", configpath, "pull" })
-if config_pull_result ~= "Already up to date.\n" then
-  print("Checking for config updates: '" .. config_pull_result .. "'")
-end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Notify the user if the config update failed. This must be done after loading
+-- LazyVim to ensure the notification displays normally.
+if config_pull_result ~= "Already up to date.\n" then
+  vim.notify(
+    "Failed to pull updates: '" .. config_pull_result .. "'",
+    vim.log.levels.ERROR,
+    { title = "LazyVim Config Update" }
+  )
+end
