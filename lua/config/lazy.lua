4602418fa31f174aa20ae6692ca8f49c10c9cea8
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-06-11 18:34:47 -0400

    feat: Add svelte language plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
