63e2241b2f854f30e1bf3d427990a920adb1d1b5
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-06-27 10:35:30 -0400

    feat: Improve messages that occur when config repository has updates

--- init.lua
+++ init.lua
@@ -11,5 +11,17 @@ require("config.lazy")
 -- Notify the user if the config update failed. This must be done after loading
 -- LazyVim to ensure the notification displays normally.
 if config_pull_result ~= "Already up to date.\n" then
-  LazyVim.notify("Tried to pull updated config: '" .. config_pull_result .. "'", { title = "LazyVim Config Update" })
+  if config_pull_result:find("Fast-forward") then
+    LazyVim.info("Successfully updated configuration. It's best to restart LazyVim.")
+  elseif config_pull_result:find("fatal") then
+    LazyVim.error(
+      "The local repository needs to be repaired before remote changes can be pulled:\n" .. config_pull_result,
+      { title = "LazyVim Config Update" }
+    )
+  else
+    LazyVim.info(
+      "Attempted to pull new changes to the user configuration:\n" .. config_pull_result,
+      { title = "LazyVim Config Update" }
+    )
+  end
 end
