366abcd0a8b3b121c92c8fab15226a4e2a98afd1
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-06-28 11:35:09 -0400

    fix: Escape the hyphen when testing if git pull was successful

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ require("config.lazy")
 -- Notify the user if the config update failed. This must be done after loading
 -- LazyVim to ensure the notification displays normally.
 if config_pull_result ~= "Already up to date.\n" then
-  if config_pull_result:find("Fast-forward") then
+  if config_pull_result:find("Fast%-forward") then
     LazyVim.info("Successfully updated configuration. It's best to restart LazyVim.")
   elseif config_pull_result:find("fatal") then
     LazyVim.error(
