e95966559d226089322cfc8459ce19104409de26
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-06-29 13:39:01 -0400

    feat: Silence "No information available" messages that have been appearing even when information is available

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,11 @@
 return {
   "folke/noice.nvim",
   opts = {
+    lsp = {
+      hover = {
+        silent = true, -- Silence the "No information available" messages
+      },
+    },
     presets = {
       lsp_doc_border = true,
     },
