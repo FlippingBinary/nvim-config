e8c7319c294dce53d0eb575e1f419ace64259740
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-07-27 17:58:46 -0400

    fix: Markdown plugin throws error with default config

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+-- Markdown preview
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = function()
+    -- The following line is a workaround from https://github.com/LazyVim/LazyVim/pull/4196
+    -- Also reported in https://github.com/iamcco/markdown-preview.nvim/issues/690
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    {
+      "<leader>cp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+}
