eab24216625a443c9094c35ba973d624b899582e
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-09-07 10:18:52 -0400

    feat: Add TreeSitter support for Leptos

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,28 +9,38 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 })
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    -- Ensure these languages are installed so I don't have to rely on autodetection
-    ensure_installed = {
-      "bash", -- because sometimes you need to bash your head against the wall
-      "c", -- because you can't spell classic without c
-      "css", -- because colors make everything better
-      "javascript", -- because who doesn't love a good semicolon joke
-      "json", -- because it's just so on point
-      "lua", -- because it's the moon of programming languages
-      "markdown", --
-      "markdown_inline", --
-      "python", -- because indentation is not a suggestion
-      "regex", --
-      "rust", -- because memory safety is no laughing matter
-      "svelte", -- because it's the new kid on the block
-      "toml", -- because it's not yaml
-      "tsx", -- because typescript + jsx = tsx
-      "typescript", -- because javascript needs some discipline
-      "vim", --
-      "wgsl", -- because webgpu shading language sounds cool
-      "yaml", -- because it's not toml
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- Ensure these languages are installed so I don't have to rely on autodetection
+      ensure_installed = {
+        "bash", -- because sometimes you need to bash your head against the wall
+        "c", -- because you can't spell classic without c
+        "css", -- because colors make everything better
+        "javascript", -- because who doesn't love a good semicolon joke
+        "json", -- because it's just so on point
+        "lua", -- because it's the moon of programming languages
+        "markdown", --
+        "markdown_inline", --
+        "python", -- because indentation is not a suggestion
+        "regex", --
+        "rust", -- because memory safety is no laughing matter
+        "svelte", -- because it's the new kid on the block
+        "toml", -- because it's not yaml
+        "tsx", -- because typescript + jsx = tsx
+        "typescript", -- because javascript needs some discipline
+        "vim", --
+        "wgsl", -- because webgpu shading language sounds cool
+        "yaml", -- because it's not toml
+      },
     },
+  },
+  {
+    "rayliwell/tree-sitter-rstml",
+    dependencies = { "nvim-treesitter" },
+    build = ":TSUpdate",
+    config = function()
+      require("tree-sitter-rstml").setup()
+    end,
   },
 }
