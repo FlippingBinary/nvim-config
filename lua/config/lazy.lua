01d1f41496fdffed94c8fb4888149600df81ba22
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2024-12-20 13:07:51 -0500

    chore: Remove redundant blink extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
