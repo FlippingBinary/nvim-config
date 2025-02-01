668ba4ba0cbbc95dfba7d8689635862b901f3a4f
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-02-01 10:54:43 -0500

    chore: Remove unused Flutter dependency

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -4,10 +4,6 @@ return {
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
-    config = function()
-      require("flutter-tools").setup({})
-    end,
   },
 }
