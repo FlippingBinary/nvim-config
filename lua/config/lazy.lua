ee3dc89e42f4af72abf77195779de6d43a7b8b2d
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-04-22 17:28:38 -0400

    feat: Add language support for Tex

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
