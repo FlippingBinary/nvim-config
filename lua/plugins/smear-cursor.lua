56a6a22af0dac986bee99d3782009a93be7c1945
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-05-25 11:11:34 -0400

    fix: Disable smear cursor on bigfile

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -2,6 +2,7 @@ return {
   {
     "sphamba/smear-cursor.nvim",
     opts = {
+      filetypes_disabled = { "bigfile" },
       min_horizontal_distance_smear = 2,
       min_vertical_distance_smear = 2,
     },
