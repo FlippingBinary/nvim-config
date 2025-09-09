d13b41e391f4f7577b0387c0b0693b90c88ad80d
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-09-09 11:49:13 -0400

    feat: Add `nixfmt` to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,8 +4,9 @@ return {
     ---@param opts ConformOpts
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.tex = { "tex-fmt" }
+      opts.formatters_by_ft.nix = { "nixfmt" }
       opts.formatters_by_ft.rust = { "rustfmt" }
+      opts.formatters_by_ft.tex = { "tex-fmt" }
     end,
   },
 }
