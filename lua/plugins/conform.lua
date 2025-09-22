1ecb722eca4fb3392ef1cdcaf31ca8bea5aa889f
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-09-22 09:54:57 -0400

    fix: Conditionally use formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,9 +4,15 @@ return {
     ---@param opts ConformOpts
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.nix = { "nixfmt" }
-      opts.formatters_by_ft.rust = { "rustfmt" }
-      opts.formatters_by_ft.tex = { "tex-fmt" }
+      if vim.g.apps.nix then
+        opts.formatters_by_ft.nix = { "nixfmt" }
+      end
+      if vim.g.apps.cargo then
+        opts.formatters_by_ft.rust = { "rustfmt" }
+      end
+      if vim.g.apps.latexmk then
+        opts.formatters_by_ft.tex = { "tex-fmt" }
+      end
     end,
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,15 +5,29 @@ return {
     -- Override the default build command because the `:MasonUpdate` fails as
     -- unavailable during installation, but becomes available later.
     build = "",
-    opts = {
-      ensure_installed = { "codelldb", "tex-fmt", "verible" },
-      automatic_installation = {
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+        "codelldb",
+        "verible",
+      })
+      if vim.g.apps.latexmk then
+        vim.list_extend(opts.ensure_installed, {
+          "tex-fmt",
+        })
+      end
+      if vim.g.apps.python then
+        vim.list_extend(opts.ensure_installed, {
+          "tombi",
+        })
+      end
+      opts.automatic_installation = {
         exclude = { "rust_analyzer" },
-      },
-      ui = {
+      }
+      opts.ui = {
         -- Put a border around the Mason window, separating it from the text behind it.
         border = "rounded",
-      },
-    },
+      }
+      return opts
+    end,
   },
 }
