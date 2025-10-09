6fbec3a866f82c1f008565824a352c4b56928f91
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-10-09 13:02:38 -0400

    chore: Remove redundant configuration

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,11 +2,6 @@ return {
   "saghen/blink.cmp",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
-  opts_extend = {
-    "sources.completion.enabled_providers",
-    "sources.compat",
-    "sources.default",
-  },
   opts = {
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
