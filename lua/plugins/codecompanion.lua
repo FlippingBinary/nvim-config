6c9bd5c46ae3136ae8380d2fde3187341bac8974
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-12-09 16:29:14 -0500

    fix: upcoming changes to CodeCompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,12 +1,3 @@
-local OllamaState = {
-  ANTICIPATING_REASONING = 1,
-  REASONING = 2,
-  ANTICIPATING_OUTPUTTING = 3,
-  OUTPUTTING = 4,
-}
----@type integer
-local _ollama_state
-
 return {
   {
     "olimorris/codecompanion.nvim",
@@ -15,69 +6,47 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
-      -- adapters = {
-      --   ollama = function()
-      --     local ollama = require("codecompanion.adapters.ollama")
-      --
-      --     return require("codecompanion.adapters").extend("ollama", {
-      --       env = {
-      --         url = "https://ai.goobygob.com:11435",
-      --       },
-      --       headers = {
-      --         ["Content-Type"] = "application/json",
-      --       },
-      --       parameters = {
-      --         sync = true,
-      --       },
-      --       schema = {
-      --         model = {
-      --           default = "gemma3:27b-it-qat",
-      --         },
-      --       },
-      --       ---Check for a token before starting the request
-      --       ---@param self CodeCompanion.Adapter
-      --       ---@return boolean
-      --       setup = function(self)
-      --         _ollama_state = OllamaState.ANTICIPATING_OUTPUTTING
-      --         return true
-      --       end,
-      --       handlers = {
-      --         chat_output = function(self, data)
-      --           local inner = ollama.handlers.chat_output(self, data)
-      --
-      --           if inner == nil then
-      --             return inner
-      --           end
-      --
-      --           if inner.status ~= "success" or inner.output == nil or type(inner.output.content) ~= "string" then
-      --             return inner
-      --           end
-      --
-      --           if string.find(inner.output.content, "<think>") ~= nil then
-      --             _ollama_state = OllamaState.ANTICIPATING_REASONING
-      --             inner.output.content = inner.output.content:gsub("%s*<think>%s*", "")
-      --           elseif string.find(inner.output.content, "</think>") ~= nil then
-      --             _ollama_state = OllamaState.ANTICIPATING_OUTPUTTING
-      --             inner.output.content = inner.output.content:gsub("%s*</think>%s*", "")
-      --           elseif inner.output.content:match("^%s*$") ~= nil then
-      --             inner.output.content = ""
-      --           elseif _ollama_state == OllamaState.ANTICIPATING_OUTPUTTING then
-      --             _ollama_state = OllamaState.OUTPUTTING
-      --           elseif _ollama_state == OllamaState.ANTICIPATING_REASONING then
-      --             _ollama_state = OllamaState.REASONING
-      --           end
-      --
-      --           if _ollama_state == OllamaState.ANTICIPATING_REASONING or _ollama_state == OllamaState.REASONING then
-      --             inner.output.reasoning = inner.output.content
-      --             inner.output.content = nil
-      --           end
-      --
-      --           return inner
-      --         end,
-      --       },
-      --     })
-      --   end,
-      -- },
+      ignore_warnings = true,
+      adapters = {
+        http = {
+          qwen3 = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "qwen3", -- Give this adapter a different name to differentiate it from the default ollama adapter
+              env = {
+                url = "https://ollama",
+              },
+              opts = {
+                vision = true,
+                stream = true,
+              },
+              schema = {
+                model = {
+                  default = "qwen3:latest",
+                },
+                num_ctx = {
+                  default = 16384,
+                },
+                think = {
+                  default = false,
+                },
+                keep_alive = {
+                  default = "5m",
+                },
+              },
+            })
+          end,
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              env = {
+                url = "https://ollama",
+              },
+              parameters = {
+                sync = true,
+              },
+            })
+          end,
+        },
+      },
       strategies = {
         chat = {
           adapter = "ollama",
@@ -104,9 +73,9 @@ return {
           },
         },
         per_filetype = {
-          codecompanion = { "codecompanion"},
-        }
+          codecompanion = { "codecompanion" },
+        },
       },
     },
-  }
+  },
 }
