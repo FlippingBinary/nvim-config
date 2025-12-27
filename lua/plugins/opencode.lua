d7106fb036b9e04e1474e26a9500f3f74d582d09
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-12-27 13:31:54 -0500

    fix: eliminate delay when pressing space in terminal or opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -24,38 +24,6 @@ return {
 
     vim.o.autoread = true
 
-    -- Wrap the provider's start/toggle to inject LazyVim root as cwd
-    local config = require("opencode.config")
-    if config.provider then
-      local original_start = config.provider.start
-      local original_toggle = config.provider.toggle
-
-      -- Get LazyVim root helper
-      local function get_root()
-        local ok, lazyvim = pcall(require, "lazyvim.util")
-        if ok then
-          return lazyvim.root.get() or vim.fn.getcwd()
-        end
-        return vim.fn.getcwd()
-      end
-
-      -- Override start to inject cwd
-      config.provider.start = function(self)
-        self.opts = self.opts or {}
-        self.opts.cwd = get_root()
-        return original_start(self)
-      end
-
-      -- Override toggle to inject cwd
-      if original_toggle then
-        config.provider.toggle = function(self)
-          self.opts = self.opts or {}
-          self.opts.cwd = get_root()
-          return original_toggle(self)
-        end
-      end
-    end
-
     -- Keymaps
     vim.keymap.set({ "n", "x" }, "<leader>aa", function()
       require("opencode").ask("@this: ", { submit = true })
@@ -65,7 +33,7 @@ return {
       require("opencode").select()
     end, { desc = "Execute opencode action" })
 
-    vim.keymap.set({ "n", "t" }, "<leader>at", function()
+    vim.keymap.set({ "n" }, "<leader>at", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
 
