ca467c009c18d4677d68397d7a4cc52119b38f57
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2026-05-09 16:48:41 -0400

    feat(lazydev): add busted and luassert types

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,29 +1,18 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       -- Mason installs a version of rust-analyzer that may not match the version installed
-  --       -- with the Rust toolchain, so this disables the mason version.
-  --       -- rust_analyzer = {
-  --       --   mason = false,
-  --       -- },
-  --       taplo = {
-  --         keys = {
-  --           {
-  --             "K",
-  --             function()
-  --               if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-  --                 require("crates").show_popup()
-  --               else
-  --                 vim.lsp.buf.hover()
-  --               end
-  --             end,
-  --             desc = "Show Crate Documentation",
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "folke/lazydev.nvim",
+    ---@module "lazydev"
+    ---@type lazydev.Config
+    opts = {
+      library = {
+        { path = "${3rd}/luassert/library", words = { "luassert" } },
+        { path = "${3rd}/busted/library", words = { "describe", "before_each", "after_each" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "LazyVim", words = { "LazyVim" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+        { path = "nvim-lspconfig", words = { "lspconfig.settings" } },
+      },
+    },
+  },
 }
